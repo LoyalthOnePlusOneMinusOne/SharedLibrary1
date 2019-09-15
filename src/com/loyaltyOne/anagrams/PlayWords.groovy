21cdd327c4857cb38d5d2f02c68131a44f07bd95
Chris Min <suntsu777@gmail.com>
2019-09-15 15:54:30 -0400

    add git source

--- src/com/loyaltyOne/anagrams/PlayWords.groovy
+++ src/com/loyaltyOne/anagrams/PlayWords.groovy
@@ -3,9 +3,51 @@ package com.loyaltyOne.anagrams;
 def debug=0
 
 def speak() {
+    stage('Initialize'){
+        deleteDir()
+    }
+
+    sh("whoami")
+    echo pwd()
+    sh("env")
+
+    stage('Checkout'){
+        dir(env.REPO_NAME) {
+            timeout(time: 600, unit: 'SECONDS') {
+                try {
+                    checkout([
+                            $class: 'GitSCM',
+                            branches: scm.branches,
+                            doGenerateSubmoduleConfigurations: scm.doGenerateSubmoduleConfigurations,
+                            extensions: scm.extensions,
+                            userRemoteConfigs: scm.userRemoteConfigs
+                    ])
+                } cache (e) {
+                    println "Error on the stage, Checkout"
+                    throw e
+                }
+            }
+        }
+    }
 
 
+    stage('Initialize'){
+    }
+    stage('Initialize'){
+    }
+    stage('Initialize'){
+    }
+    stage('Initialize'){
+    }
+    stage('Initialize'){
+    }
+    stage('Initialize'){
+    }
+    stage('Initialize'){
+    }
+    stage('Initialize'){
+    }
 
-}
+} //end of speak
 
 
